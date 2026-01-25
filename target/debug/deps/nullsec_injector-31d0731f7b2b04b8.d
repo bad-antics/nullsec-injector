c26fba383e1a5b2ac4d50fa98dcd928d4d8c8cfa
bad-antics <bad-antics@proton.me>
2026-01-25 09:38:28 -0800

    fix: exclude target from package

--- target/debug/deps/nullsec_injector-31d0731f7b2b04b8.d
+++
@@ -1,5 +0,0 @@
-/home/antics/nullsec-repos/nullsec-injector/target/debug/deps/nullsec_injector-31d0731f7b2b04b8.d: src/main.rs
-
-/home/antics/nullsec-repos/nullsec-injector/target/debug/deps/nullsec_injector-31d0731f7b2b04b8: src/main.rs
-
-src/main.rs:
--- target/package/nullsec-injector-2.0.0/target/release/nullsec-injector.d
+++
@@ -1,1 +0,0 @@
-/home/antics/nullsec-repos/nullsec-injector/target/release/nullsec-injector: /home/antics/nullsec-repos/nullsec-injector/src/main.rs
